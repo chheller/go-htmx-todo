18f5e4e19121f3dbd4ef33b47e63b8d68ebbe3c4
Charles Heller <charleshh@protonmail.com>
2024-04-28 15:42:38 -0500

    make websocket connection close when the client closes

--- modules/router/router.go
+++ modules/router/router.go
@@ -6,6 +6,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/chheller/go-htmx-todo/modules/config"
 	"github.com/chheller/go-htmx-todo/modules/domain"
 	"github.com/chheller/go-htmx-todo/modules/user"
 	"github.com/chheller/go-htmx-todo/modules/web"
@@ -27,17 +28,44 @@ func CreateRouter(services *ApplicationServices) *http.ServeMux {
 	staticFileServer := http.FileServer(http.FS(static))
 	router.Handle("/static/", http.StripPrefix("/static/", staticFileServer))
 
-	// Add no-op websocket handler for the reloadBrowser.js functionality
-	router.Handle("/ws", websocket.Handler(func(ws *websocket.Conn) {
-		log.Info("Websocket connection established")
-		defer log.Info("Websocket connection closed")
-		defer ws.Close()
+	if config.GetEnvironment().InjectBrowserReload {
+		// Add no-op websocket handler for the reloadBrowser.js functionality
+		router.Handle("/ws", websocket.Handler(func(ws *websocket.Conn) {
+			log.Info("Websocket connection established")
+			defer log.Info("Websocket connection closed")
+			defer ws.Close()
 
-		stop := make(chan os.Signal, 1)
-		signal.Notify(stop, os.Interrupt)
-		signal.Notify(stop, syscall.SIGTERM)
-		<-stop
-	}))
+			// Create a channel to listen on the websocket connection
+			websocketClosed := make(chan uint)
+
+			// Start a goroutine to read messages from the websocket connection
+			go func() {
+				for {
+					err := websocket.Message.Receive(ws, &struct{}{})
+					if err != nil {
+						log.Info("Websocket connection closed by client")
+						websocketClosed <- 0
+						break
+					}
+				}
+			}()
+
+			// Wait for the websocket connection to close
+			stop := make(chan os.Signal, 1)
+			signal.Notify(stop, os.Interrupt)
+			signal.Notify(stop, syscall.SIGTERM)
+			select {
+			case <-websocketClosed:
+				{
+
+				}
+			case <-stop:
+				{
+
+				}
+			}
+		}))
+	}
 
 	// Setup application handlers
 	handlers := []domain.Handler{
